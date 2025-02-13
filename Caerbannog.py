146d28fa96f57bb1f2578e5d26180c51c0efbe99
Ascellayn <87911136+SiriusBYT@users.noreply.github.com>
2025-02-13 17:13:43 +0100

    Slow as shit and has debug configuration issues but works otherwise

--- Caerbannog.py
+++ Caerbannog.py
@@ -3,10 +3,11 @@
 The Sirio Network License 1.0 - Freely Derivable, Non-Commercial.  
 *Origin: https://dev.sirio-network.com/license/1.0*
 """
+from TSN_Abstracter import *;
 
-import watchdog.events
-import watchdog.observers
-from CEBN_Dependencies import *;
+import watchdog.events;
+import watchdog.observers;
+
 
 import websockets;
 import threading;
@@ -27,8 +28,8 @@ Clients = [];
 Reload_Completed = [];
 
 """ Websocket Server """
-def WebSocket_Server() -> None:
-    Log(f'[wS Server] Starting WebSockets Server...');
+def WebSocket_Server() -> None: # NOTE: This will be later replaced by the new Trinity code, eventually.
+    S_Web = Log.Info(f'Starting WebSockets Server...');
 
     async def Websocket_Handler(Client) -> None:
         global Clients, Reload_Completed, Reload_Status;
@@ -37,47 +38,44 @@ def WebSocket_Server() -> None:
         Client_Address = str(Address[0])+":"+str(Address[1]);
         Clients.append(Client);
 
-        Log(f'[WS Server] Connection: Web://{Client_Address}.');
-        while (True):
-            if (Running):
-                try:
-                    if (Reload_Status == True and Client not in Reload_Completed):
-                        Reload_Result = await Reload_CSS(Client);
-                        if (Reload_Result):
-                            Reload_Completed.append(Client);
-                        else: return;
-                    
-                    if (len(Reload_Completed) == len(Clients)):
-                        Reload_Status = False;
-                        Reload_Completed = []; 
+        Log.Info(f'Connection: Web://{Client_Address}.');
+        while (Running):
+            try:
+                if (Reload_Status == True and Client not in Reload_Completed):
+                    Reload_Result = await Reload_CSS(Client);
+                    if (Reload_Result):
+                        Reload_Completed.append(Client);
+                    else: return;
                 
-                    #Log(f"DEBUG: {Reload_Completed}({len(Reload_Completed)}) {Clients}({len(Clients)})")
-                
-                except websockets.ConnectionClosed:
-                    Log(f'[Connection] CLOSED: Web://{Client_Address}.');
-                    Clients.remove(Client);
-                    return;
-            else:
+                if (len(Reload_Completed) == len(Clients)):
+                    Reload_Status = False;
+                    Reload_Completed = []; 
+            
+                #Log.Debug(f"{Reload_Completed}({len(Reload_Completed)}) {Clients}({len(Clients)})")
+            
+            except websockets.ConnectionClosed:
+                Log.Info(f'CLOSED: Web://{Client_Address}.');
                 Clients.remove(Client);
                 return;
-            await asyncio.sleep(0);
+        Clients.remove(Client);
+        return;
 
-    async def Websocket_Listener() -> None:
-        Log(f'[WS Server] Listening for WebSockets connections.');
+    async def Websocket_Listener(S_Web: Log.Awaited_Log) -> None:
         async with websockets.serve(Websocket_Handler, "127.0.0.1", 6701) as cebn_server:
-            await cebn_server.serve_forever();
-    
-    asyncio.run(Websocket_Listener());
+            S_Web.OK();
+            await cebn_server.serve_forever() 
+
+    asyncio.run(Websocket_Listener(S_Web));
 
 """" Watch Logic """
 async def Reload_CSS(Client) -> bool:
-    Log(f'[WS Server] Calling to reload QuickCSS...');
+    S_Call = Log.Info(f'Calling to reload QuickCSS...');
     try:
         await Client.send("WAKE UP MOTHERFUCKER");
-        Log(f'[WS Server] Successfully sent reload request.');
+        S_Call.OK();
         return True;
-    except:
-        Log(f'[WS Server] Error asking to reload QuickCSS.');
+    except Exception as Except:
+        S_Call.ERROR(Except);
         return False;
 
 def Fetch_CSS(Folder_Name: str, Config_CSS: str) -> list:
@@ -92,7 +90,7 @@ def Fetch_CSS(Folder_Name: str, Config_CSS: str) -> list:
         Allow_Append = True;
         Relative_Path = Root_Path.replace(os.getcwd(), "")[1:].replace("\\","/");
         if (Continue_Search):
-            #Log(f"Relative_Path: {Relative_Path}\nFolders: {Folders}\nFiles: {Files};");
+            Log.Debug(f"Relative_Path: {Relative_Path}\nFolders: {Folders}\nFiles: {Files};");
             for File in Files:
                 for BF in Blacklisted_Files:
                     if (File == BF): Allow_Append = False;
@@ -115,16 +113,22 @@ def Combine_CSS(CSS_Files: list) -> str:
 
 def Compile_CSS(Combined: str) -> str:
     # The Flashcord Compiler will need to be better than this pile of shit!
-    Log(f"Compiling CSS...");
+    S_Compile = Log.Debug(f"Compiling CSS...");
     Compiled = Combined;
 
-    Log(f"Removing Whitespaces at the start of each line...")
+    S_RW = Log.Debug(f"Removing Whitespaces at the start of each line...");
     Compiled = re.sub("(^[  ]*)", "", string=Compiled, flags=re.MULTILINE);
-    Log(f"Removing ALL Line Breaks...")
-    Compiled = Compiled.replace("\n", "")
-    Log(f"Removing comments...")
-    Compiled = re.sub("/\\*.*?\\*/", "", string=Compiled, flags=re.MULTILINE);
+    S_RW.OK();
 
+    S_LN = Log.Debug(f"Removing ALL Line Breaks...");
+    Compiled = Compiled.replace("\n", "");
+    S_LN.OK();
+
+    S_Comments = Log.Debug(f"Removing comments...")
+    Compiled = re.sub("/\\*.*?\\*/", "", string=Compiled, flags=re.MULTILINE);
+    S_Comments.OK();
+
+    S_Compile.OK();
     return Compiled;
 
 def Root_Options(Options: list) -> str:
@@ -135,21 +139,23 @@ def Root_Options(Options: list) -> str:
     return CSS;
 
 def Finalize_CSS(Compiled: str, Folder_Name: str, Config_CSS: str) -> str:
-    Log(f"Finalizing Caerbannog Compilation...");
+    S_Finalize = Log.Debug(f"Finalizing Caerbannog Compilation...");
 
-    with open(Configuration["License_File"], "r", encoding="UTF-8") as LSC:
-        License = LSC.read();
-    with open(f"{Folder_Name}/{Config_CSS}", "r", encoding="UTF-8") as CFG:
-        CFG_CSS = CFG.read();
+    License = File.Read(Configuration["License_File"]);
+    CFG_CSS = File.Read(f"{Folder_Name}/{Config_CSS}");
 
     Compiler_Notes = f"/* Compiled using Caerbannog {Caerbannog_Version}*/\n"
 
     Root_Footer = [];
     Root_Footer.append(["Caerbannog-Version", Caerbannog_Version]);
-    Root_Footer.append(["Caerbannog-Compile_Date", Date_String()]);
+    Root_Footer.append(["Caerbannog-Compile_Date", Time.Get_DateStrings(Time.Get_Unix())]);
     Root_Footer = Root_Options(Root_Footer);
 
     Compiler_Notes += Root_Footer;
+
+    # Making the License inclusion less retarded, only allow the first 3 lines of it.
+    License_Array = License.split("\n");
+    License = f"{License_Array[0]}\n{License_Array[1]}\n{License_Array[2]}"; # I'm lazy, fuck you.
 
     Production = f"\
 /*\n\
@@ -165,6 +171,7 @@ def Finalize_CSS(Compiled: str, Folder_Name: str, Config_CSS: str) -> str:
 {CFG_CSS}\n\n\
 {Compiler_Notes}"
 
+    S_Finalize.OK();
     return Production, Development;
 
 class Watchdog_Handler(watchdog.events.FileSystemEventHandler):
@@ -173,43 +180,60 @@ class Watchdog_Handler(watchdog.events.FileSystemEventHandler):
         self.Config_CSS = Config_CSS;
         self.Dev_CSS = Dev_CSS;
         self.Prod_CSS = Prod_CSS;
-        Log(f"[{Folder_Name}] Added watchdog with Config_CSS: {Config_CSS}; Dev_CSS: {Dev_CSS}; Prod_CSS: {Prod_CSS}.");
+        Log.Info(f"[{Folder_Name}] Added watchdog with Config_CSS: {Config_CSS}; Dev_CSS: {Dev_CSS}; Prod_CSS: {Prod_CSS}.");
     
     def on_any_event(self, event) -> None:
         global Reload_Status, Reload_Completed;
-        Log(f"New recorded event: {event}, triggering recompilation for {self.Folder_Name}!\n");
+        Log.Info(f"Triggering recompilation for {self.Folder_Name}:\n\tEvent: {event}");
 
-        Log(f"[{self.Folder_Name}] Fetching CSS...")
+        S_Fetch = Log.Debug(f"[{self.Folder_Name}] Fetching CSS...");
         CSS_Files = Fetch_CSS(self.Folder_Name, self.Config_CSS);
-        Log(f"[{self.Folder_Name}] Combining CSS..")
+        S_Fetch.OK()
+
+        S_Combine = Log.Debug(f"[{self.Folder_Name}] Combining CSS...");
         Combined = Combine_CSS(CSS_Files);
-        Log(f"[{self.Folder_Name}] Compiling CSS...")
+        S_Combine.OK();
+
+        S_Compile = Log.Debug(f"[{self.Folder_Name}] Compiling CSS...");
         Compiled = Compile_CSS(Combined);
-        Log(f"[{self.Folder_Name}] Finalizing CSS...")
+        S_Compile.OK();
+
+        S_Finalize = Log.Debug(f"[{self.Folder_Name}] Finalizing CSS...")
         Production, Development = Finalize_CSS(Compiled, self.Folder_Name, self.Config_CSS);
-        Log(f"[{self.Folder_Name}] Writing CSS...")
-        with open(self.Dev_CSS, "w", encoding="UTF-8") as Development_CSS:
-            Development_CSS.write(Development);
-        with open(self.Prod_CSS, "w", encoding="UTF-8") as Production_CSS:
-            Production_CSS.write(Production);
-        Log(f"{self.Folder_Name} has been recompiled.")
+        S_Finalize.OK();
+
+        S_Write = Log.Debug(f"[{self.Folder_Name}] Writing CSS...")
+        try:
+            with open(self.Dev_CSS, "w", encoding="UTF-8") as Development_CSS:
+                Development_CSS.write(Development);
+            with open(self.Prod_CSS, "w", encoding="UTF-8") as Production_CSS:
+                Production_CSS.write(Production);
+            S_Write.OK();
+            Log.Info(f"{self.Folder_Name} has been recompiled.");
+        except Exception as Except:
+            S_Write.ERROR(Except);
+        
         # This may look stupid but there's a fucking race condition here
         Reload_Status = False;
         Reload_Completed = [];
         Reload_Status = True;
-        
 
-async def Bootstrap() -> None:
+async def Bootstrap(S_Start: Log.Awaited_Log) -> None:
     global Configuration;
     
-    Log(f'[Bootstrap] Loading configuration...');
-    Configuration = JSON_Load("Caerbannog.json");
-    Log(f'[Bootstrap] Starting threads...');
-    threading.Thread(target=WebSocket_Server).start();
-    Log(f'[Bootstrap] Changing Directories...');
-    os.chdir("..");
+    S_CFG = Log.Info(f'Loading configuration...');
+    Configuration = File.JSON_Load("Caerbannog.json");
+    Log.Debug(f"Loaded Configuration:\n{Configuration}");
+    S_CFG.OK();
 
-    Log(f'[Bootstrap] Starting Watchdog...');
+    S_Thread = Log.Info(f'Starting threads...');
+    WebSockets_Thread = threading.Thread(target=WebSocket_Server);
+    WebSockets_Thread.daemon = True;
+    WebSockets_Thread.start();
+    S_Thread.OK();
+
+    S_Watch = Log.Info(f'Starting Watchdog...');
+    os.chdir("..");
     Observers = [];
     Index = -1;
     for Folder in Configuration["Watchdog"].keys():
@@ -218,27 +242,32 @@ async def Bootstrap() -> None:
         Observers.append(watchdog.observers.Observer());
         Observers[Index].schedule(Watchdog_Handler(Folder, Folder_Dict["Config_CSS"], Folder_Dict["Dev_CSS"], Folder_Dict["Prod_CSS"]), Folder, recursive=True);
         Observers[Index].start();
+    S_Watch.OK();
 
-    """
-    Observe_CSS = watchdog.observers.Observer();
-    Observe_CSS.schedule(Watchdog_Handler(), Configuration["Folder"], recursive=True);
-    Observe_CSS.start();
-    """
-    Log(f'[Bootstrap] Caerbannog is now ready.');
+    S_Start.OK();
     while True:
         try:
-            Void();
-        except KeyboardInterrupt:
+            if (Reload_Status):
+                Log.Carriage(f"{Caerbannog_Version} - Reloaded Clients: {len(Reload_Completed)}/{len(Clients)}");
+            else:
+                Log.Carriage(f"{Caerbannog_Version} - Connected Clients: {len(Clients)}");
+            await asyncio.sleep(1);
+        except:
             Shutdown();
-        await asyncio.sleep(1);
 
 def Shutdown() -> None:
     global Running;
-    Log(f'Shutting down Caerbannog...');
+    Log.Critical(f"=== Shutting down Caerbannog ===");
     Running = False;
     quit();
 
+
+# Uncomment this for basically debug mode
+"""Log.Delete();
+Config.Logging["File"] = True;
+Config.Logging["File_Level"] = 0;
+Config.Logging["Print_Level"] = 0;"""
+
 # Spark
-if __name__== '__main__': 
-   Log(f'[Spark] Starting Caerbannog...');
-   asyncio.run(Bootstrap());
+if __name__ == '__main__': 
+   asyncio.run(Bootstrap(Log.Info(f'Starting Caerbannog...')));
